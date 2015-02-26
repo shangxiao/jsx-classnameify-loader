110dfc07697bfaa4b95e5f771e55f4540232486e
David Sanders <dsanders@rapilabs.com>
2015-02-27 06:00:27 +1100

    Extended to replace for -> htmlFor as well

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 module.exports = function(source) {
   this.cacheable && this.cacheable();
-  return source.replace(/([^0-9a-zA-Z_$])class=/g, '$1className=');
+  return source
+    .replace(/([^0-9a-zA-Z_$])class=/g, '$1className=')
+    .replace(/([^0-9a-zA-Z_$])for=/g, '$1htmlFor=');
 };
