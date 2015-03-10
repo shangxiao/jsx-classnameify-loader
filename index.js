5c053e788c33f1bb3a9f6a4356c7acd1a8103b1d
David Sanders <dsanders@rapilabs.com>
2015-03-11 03:50:04 +1100

    Added all supported camel case attributes

--- index.js
+++ index.js
@@ -1,6 +1,57 @@
+
+var translations = {
+
+  // JavaScript keywords
+  'class': 'className',
+  'for':   'htmlFor',
+
+  // attributes requiring camel case
+  // see http://facebook.github.io/react/docs/tags-and-attributes.html#html-attributes
+  'acceptcharset':     'acceptCharset',
+  'accesskey':         'accessKey',
+  'allowfullscreen':   'allowFullScreen',
+  'allowtransparency': 'allowTransparency',
+  'autocomplete':      'autoComplete',
+  'autofocus':         'autoFocus',
+  'autoplay':          'autoPlay',
+  'cellpadding':       'cellPadding',
+  'cellspacing':       'cellSpacing',
+  'charset':           'charSet',
+  'classid':           'classID',
+  'colspan':           'colSpan',
+  'contenteditable':   'contentEditable',
+  'contextmenu':       'contextMenu',
+  'crossorigin':       'crossOrigin',
+  'datetime':          'dateTime',
+  'enctype':           'encType',
+  'formaction':        'formAction',
+  'formenctype':       'formEncType',
+  'formmethod':        'formMethod',
+  'formnovalidate':    'formNoValidate',
+  'formtarget':        'formTarget',
+  'frameborder':       'frameBorder',
+  'hreflang':          'hrefLang',
+  'httpequiv':         'httpEquiv',
+  'marginheight':      'marginHeight',
+  'marginwidth':       'marginWidth',
+  'maxlength':         'maxLength',
+  'mediagroup':        'mediaGroup',
+  'novalidate':        'noValidate',
+  'radiogroup':        'radioGroup',
+  'readonly':          'readOnly',
+  'rowspan':           'rowSpan',
+  'spellcheck':        'spellCheck',
+  'srcdoc':            'srcDoc',
+  'srcset':            'srcSet',
+  'tabindex':          'tabIndex',
+  'usemap':            'useMap',
+};
+
 module.exports = function(source) {
   this.cacheable && this.cacheable();
-  return source
-    .replace(/([^0-9a-zA-Z_$])class=/g, '$1className=')
-    .replace(/([^0-9a-zA-Z_$])for=/g, '$1htmlFor=');
+  for (from in translations) {
+    var regex = new RegExp('([^0-9a-zA-Z_$])' + from + '=', 'g')
+    source = source.replace(regex, '$1' + translations[from] + '=')
+  }
+  return source;
 };
